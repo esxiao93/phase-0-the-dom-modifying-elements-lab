83737c214002c2ecf4a3a5da309097dd6c661a3b
Eric Xiao <es.xiao93@gmail.com>
2022-04-14 15:56:09 -0400

    commit tree 48a8d7be389da7422ec93362015d4e35050ef3a1

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your code here!
+const main = document.getElementById('main');
+//console.log(main)
+main.remove();
+
+const newHeader = document.createElement('h1');
+const body = document.querySelector('body');
+body.append(newHeader);
+newHeader.nodeName = 'H1'
+newHeader.id = 'victory'
+newHeader.innerHTML = 'ERIC is the champion'
+
+
+
+
+
+
+
+// 1) has a 'newHeader' variable that points to node 'h1#victory'
+// 2) has a 'newHeader' variable that points to node 'h1#victory'
+// 3) has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside
